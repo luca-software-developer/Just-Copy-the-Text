eedbb63a6243b3990f5a57e22466d19cea95f614
Luca Dello Russo <lucadellorusso5@gmail.com>
2024-02-20 19:06:10 +0100

    Manutenzione evolutiva

--- JustCopyTheText/src/main/java/justcopythetext/JustCopyTheTextController.java
+++ JustCopyTheText/src/main/java/justcopythetext/JustCopyTheTextController.java
@@ -11,9 +11,13 @@ import javafx.scene.control.Button;
 import javafx.scene.input.Clipboard;
 import javafx.scene.input.ClipboardContent;
 import javax.swing.text.BadLocationException;
-import javax.swing.text.Document;
 import javax.swing.text.rtf.RTFEditorKit;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
+import org.jsoup.parser.Tag;
+import org.jsoup.select.NodeTraversor;
+import org.jsoup.select.NodeVisitor;
 
 /**
  * La classe {@code JustCopyTheTextController} rappresenta il controller MVC
@@ -39,18 +43,60 @@ public class JustCopyTheTextController implements Initializable {
 
     @FXML
     private void removeFormatting(ActionEvent actionEvent) throws IOException, BadLocationException {
-        if (clipboard.hasHtml()) {
-            final ClipboardContent clipboardContent = new ClipboardContent();
-            clipboardContent.putString(Jsoup.parse(clipboard.getHtml()).text());
-            clipboard.setContent(clipboardContent);
-        } else if (clipboard.hasRtf()) {
-            final RTFEditorKit rtfEditorKit = new RTFEditorKit();
-            final Document document = rtfEditorKit.createDefaultDocument();
-            final ClipboardContent clipboardContent = new ClipboardContent();
-            rtfEditorKit.read(new ByteArrayInputStream(clipboard.getRtf().getBytes()), document, 0);
-            clipboardContent.putString(document.getText(0, document.getLength()));
+        final ClipboardContent clipboardContent = new ClipboardContent();
+        if (clipboard.hasHtml() || clipboard.hasRtf()) {
+            clipboardContent.putString(
+                    clipboard.hasHtml()
+                    ? removeFormattingHTML(clipboard.getHtml())
+                    : removeFormattingRTF(clipboard.getRtf()));
             clipboard.setContent(clipboardContent);
         }
+    }
+
+    private String removeFormattingHTML(String html) {
+        FormattingVisitor formatter = new FormattingVisitor();
+        NodeTraversor.traverse(formatter, Jsoup.parse(html));
+        return formatter.toString();
+    }
+
+    private String removeFormattingRTF(String rtf) throws IOException, BadLocationException {
+        final RTFEditorKit rtfEditorKit = new RTFEditorKit();
+        final javax.swing.text.Document document = rtfEditorKit.createDefaultDocument();
+        rtfEditorKit.read(new ByteArrayInputStream(rtf.getBytes()), document, 0);
+        return document.getText(0, document.getLength());
+    }
+
+    private static class FormattingVisitor implements NodeVisitor {
+
+        private final StringBuilder stringBuilder;
+
+        public FormattingVisitor() {
+            stringBuilder = new StringBuilder();
+        }
+
+        @Override
+        public void head(Node node, int depth) {
+            if (node instanceof TextNode) {
+                stringBuilder.append(((TextNode) node).text());
+            } else if (Tag.valueOf(node.nodeName()).isBlock()) {
+                if (!node.nodeName().equals("li")) {
+                    stringBuilder.append("\n");
+                }
+            }
+        }
+
+        @Override
+        public void tail(Node node, int depth) {
+            if (Tag.valueOf(node.nodeName()).isBlock()) {
+                stringBuilder.append("\n");
+            }
+        }
+
+        @Override
+        public String toString() {
+            return stringBuilder.toString().trim();
+        }
+
     }
 
 }
